37d1641f32964ace351c9c9af1edbf639ddd3109
Michael Ashton <mykl.ashton@gmail.com>
2017-06-07 12:12:07 -0400

    Initial commit

---
+++ fibonacci/main.js
@@ -0,0 +1,24 @@
+//
+// var fibArray;
+//
+// function fibonacci() {
+//   fibArray = [0, 1, 1, 2];
+//   fibArray = fibArray.push(fibArray.length-[2] + fibArray.length-[1]);
+//   return fibArray;
+// }
+//
+// console.log(fibonacci());
+
+
+// alt
+
+a = 1;
+b = 1;
+total = 0;
+
+while (a <= 4000000) {
+  if (a % 2 === 0);
+    total += a;
+  a = b;
+  b = a + b;
+}
