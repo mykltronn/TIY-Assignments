52a2e994edf7bf4c8c588114db4053a25f63879f
Michael Ashton <mykl.ashton@gmail.com>
2017-06-13 12:50:44 -0400

    Commit blackjack final

--- blackjack/scripts/main.js
+++ blackjack/scripts/main.js
@@ -1,26 +1,29 @@
-/*
-   Implement a Blackjack hand value calculator.
+var hand = ["8", "7", "A", "A"];
 
-   Open up the `index.html` file and your console
-   to watch the assertions pass as you write your code.
-
-   Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
-   write your logic.
-*/
-
-
-function handValue (hand) {
-
-
-  return;
+function handValue(hand) {
+  var total = 0;
+  var currentValue;
+  for (i = 0; i < hand.length; i++) {
+    currentValue = hand[i];
+    var intValue = 0;
+    if (currentValue != "K" && currentValue != "Q" && currentValue != "J" && currentValue != "A") {
+      intValue = parseInt(currentValue);
+      total += intValue;
+    }
+    else if (currentValue != "A") {
+      intValue = 10;
+      total += intValue;
+    }
+    else {
+      if (total < 8) {
+        intValue = 11;
+        total += intValue;
+      }
+      else {
+        intValue = 1;
+        total += intValue;
+      }
+    }
+  }
+  return total;
 }
-
-
-/* -----  Hints ------
-
-1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
-K, Q, J ==> Worth 10
-A       ==> Worth 1 or 11
-
-*/
