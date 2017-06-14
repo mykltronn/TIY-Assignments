bf6674ddaf3e121eb04f1ef5b93e4496eff16fc5
Michael Ashton <mykl.ashton@gmail.com>
2017-06-14 08:07:51 -0400

    initial chess commit

---
+++ chessboard/scripts/main.js
@@ -0,0 +1,39 @@
+// var board = [
+//   ['R','N','B','Q','K','B','N','R'],
+//   ['P','P','P','P','P','P','P','P'],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   ['p','p','p','p','p','p','p','p'],
+//   ['r','n','b','q','k','b','n','r'] ];
+//
+// console.log(board.join('\n') + '\n\n');
+//
+// // Move King's Pawn forward 2
+// board[4][4] = board[6][4];
+// board[6][4] = ' ';
+// console.log(board.join('\n'));
+//
+// vvvvvv output vvvvvv
+// R,N,B,Q,K,B,N,R
+// P,P,P,P,P,P,P,P
+//  , , , , , , ,
+//  , , , , , , ,
+//  , , , , , , ,
+//  , , , , , , ,
+// p,p,p,p,p,p,p,p
+// r,n,b,q,k,b,n,r
+//
+// R,N,B,Q,K,B,N,R
+// P,P,P,P,P,P,P,P
+//  , , , , , , ,
+//  , , , , , , ,
+//  , , , ,p, , ,
+//  , , , , , , ,
+// p,p,p,p, ,p,p,p
+// r,n,b,q,k,b,n,r
+
+
+//question is, how to turn a 2D array in to commands for html objects
+// the
